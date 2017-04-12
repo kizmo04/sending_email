8593d4bc055f92c6c0fb7276eb17a8d57114acb4
kizmo04 <kizmo04@gmail.com>
2017-04-13 04:22:58 +0900

    request method POST로 변경

--- django_app/sending_email/views.py
+++ django_app/sending_email/views.py
@@ -1,14 +1,24 @@
 from django.core.mail import send_mail
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 
 def index(request):
-    send_mail(
-        '테스트입니다',
-        '안녕?',
-        'kizmo04@gmail.com',
-        ['30sexytori@gmail.com'],
-        fail_silently=True,
-    )
+    # send_mail(
+    #     '테스트입니다',
+    #     '안녕?',
+    #     'kizmo04@gmail.com',
+    #     ['30sexytori@gmail.com'],
+    #     fail_silently=True,
+    # )
+    if request.method == 'POST':
+        subject = request.POST.get('subject', '')
+        message = request.POST.get('message', '')
+        from_email = request.POST.get('from_email', '')
+        if subject and message and from_email:
 
-    return render(request, 'common/index.html')
+        return redirect()
+    else:
+        context = {
+
+        }
+    return render(request, 'common/index.html', context)
